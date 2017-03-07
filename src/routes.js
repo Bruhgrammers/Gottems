40f4c964e634c50125e866e327f7b3e2c26e1d07
Veritas Prep <veritasprep@Veritass-MacBook-Pro.local>
2017-03-06 23:16:03 -0800

    changes

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     return (
       <MainContainer {...this.props}>
         <div id='body'>
-        Hello, World
+        Hello, World   changes
           <Grid>
             <Row>
               <Col xs={12}>
